4bfd8c4238d029e9013a37abb9330e2ddec97251
Daniel Semblano <dsemblano@gmail.com>
2024-02-27 13:02:25 -0300

    first commit, a draft about the website

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,16 @@
 import { z, defineCollection } from "astro:content";
+const servicosSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+        message: 'tags must be unique',
+    }).optional(),
+});
+
 const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -24,13 +36,16 @@ const storeSchema = z.object({
     heroImage: z.string().optional(),
 });
 
+export type ServicosSchema = z.infer<typeof servicosSchema>;
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 
+const servicosCollection = defineCollection({ schema: servicosSchema });
 const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = {
+    'servicos': servicosCollection,
     'blog': blogCollection,
     'store': storeCollection
 }
