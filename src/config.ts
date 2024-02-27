1454f2c6d267644376b41eee64b834707ef14f3b
Daniel Semblano <dsemblano@gmail.com>
2024-02-27 13:17:32 -0300

    Thieme changed to aqua

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tino Viana | Consultoria & Produções vastro';
+export const SITE_TITLE = 'Tino Viana | Consultoria & Produções';
 export const SITE_DESCRIPTION = 'A Tino Viana Consultoria & Produções, sediada em Curitiba/PR, tem enorme experiência em consultoria organizacional e em artes cênicas.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
