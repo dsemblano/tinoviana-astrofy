58c7f7585620e45f6460c49fae79eace4138edd2
Daniel Semblano <dsemblano@gmail.com>
2024-03-04 15:38:46 -0300

    New color: gold

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				gold : '#d4a259'
+			}
+		},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
