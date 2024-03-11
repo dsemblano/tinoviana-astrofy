b1e9faf6e33bd21c67bc994f33ec9ce6be06f06d
Daniel Semblano <dsemblano@gmail.com>
2024-03-11 19:00:44 -0300

    SEO: New site description

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tino Viana | Consultoria & Produções';
-export const SITE_DESCRIPTION = 'A Tino Viana Consultoria & Produções, sediada em Curitiba/PR, tem enorme experiência em consultoria organizacional e em artes cênicas.';
+export const SITE_TITLE = 'Tino Viana | Consultoria, Produções & Artes Cênicas';
+export const SITE_DESCRIPTION = 'Metodologia construtivista para todas empresas. Palestras, coaching, mentoria, cursos de conversação em inglês e francês e artes cênicas.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
