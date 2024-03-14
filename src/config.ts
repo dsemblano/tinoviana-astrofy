7dd9bd350febb0f8241541f99a293f6d67d8f28f
Daniel Semblano <dsemblano@gmail.com>
2024-03-14 14:44:06 -0300

    SEO: changed meta description and photo

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Tino Viana | Consultoria, Produções & Artes Cênicas';
-export const SITE_DESCRIPTION = 'Metodologia construtivista para todas empresas. Palestras, coaching, mentoria, cursos de conversação em inglês e francês e artes cênicas.';
+export const SITE_DESCRIPTION = 'Metodologia construtivista para empresas. Palestras, coaching, mentoria, cursos de conversação (inglês e francês), artes cênicas e muito mais!';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
