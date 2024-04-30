fa2e81bb67cb9ede8421130687b96bf24b1d1444
Daniel Semblano <dsemblano@gmail.com>
2024-04-30 20:04:53 -0300

    changed url path gallery photos

---
+++ .vercel/output/functions/_render.func/home/daniel/projects/Astro/AstroTino/tinoviana-astrofy/.vercel/output/_functions/chunks/_slug__H61haeg-.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_Bl4ooGp_.mjs').then(n => n.b);
+
+export { page };
--- .vercel/output/functions/_render.func/home/daniel/projects/Astro/AstroTino/tinoviana-astrofy/.vercel/output/_functions/chunks/services_DsEXWddj.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_BuW4_3iz.mjs').then(n => n.s);
-
-export { page };
