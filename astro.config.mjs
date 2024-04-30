056ec4b2391b73ed4bec11ad987d6b3894c953dd
Daniel Semblano <dsemblano@gmail.com>
2024-04-30 15:19:06 -0300

    minor ajustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
-import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tinoviana.com.br',
-  integrations: [mdx(), sitemap(), tailwind(), partytown()],
-  output: "server",
-  adapter: vercel()
+  integrations: [mdx(), sitemap(), tailwind(), partytown()]
 });
